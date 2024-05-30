5096481f0d73c4897902715d071b362450506d62
Sam <beardmans72@gmail.com>
2024-05-31 00:39:32 +1200

    fixes

--- infrastructure/modules/active_assignment/main.tf
+++ infrastructure/modules/active_assignment/main.tf
@@ -1,3 +1,4 @@
+
 data "azuread_group" "role_group" {
   display_name = var.group_name
 }
@@ -8,19 +9,20 @@ resource "azurerm_pim_active_role_assignment" "assignment" {
   scope              = var.scope
   justification      = var.justification
 
-  # Not using ticket data but the current azurerm version
-  # will force a recreate each time if you do not set this
-  # as empty.
-  ticket {}
-
-  # If a role has no maximum duration, setting "0"
-  # will make it a permamnent assignment. If there is
-  # a maximum duration, it will throw a cryptic error
-  # about the maximum duration needing to be more than
-  # five minutes.
   schedule {
     expiration {
-      end_date_time = coalesce(var.expiration_date, 0)
+      end_date_time = var.expiration_date
     }
   }
+
+  lifecycle {
+    replace_triggered_by = [terraform_data.force_replacement.id]
+  }
+}
+
+resource "terraform_data" "force_replacement" {
+
+  triggers_replace = [
+    var.expiration_date
+  ]
 }
--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.7.5"
   required_providers {
-    
+
     # Locking azurerm to a version here as the PIM APIs 
     # and resources have been buggy and required changes
     # in the past
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.104"
+      version = "= 3.103"
     }
     azuread = {
       source  = "hashicorp/azuread"
